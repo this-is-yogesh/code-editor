02448cfe86a77595d694651325c6d1ec47fcc68a
Yogesh Singh <ayogesh1214@gmail.com>
2025-02-09 15:36:28 +0530

    footer ui developed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ConvexClientProvider } from "./ConvexClientProvider";
+import Footer from "@/components/Footer";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -31,10 +32,11 @@ export default function RootLayout({
     >
       <html lang="en">
         <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+          className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen`}
         >
           <ConvexClientProvider>{children}</ConvexClientProvider>
         </body>
+        <Footer />
       </html>
     </ClerkProvider>
   );
---
+++ src/components/Footer.tsx
@@ -0,0 +1,34 @@
+import { Blocks } from "lucide-react";
+import Link from "next/link";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <>
+      <footer className="parentFooter">
+        <div className="insideFirstBox">
+          <div className="buildfordeveloperbox">
+            <Blocks className="size-5" />
+            <p> Build for developers, by developers.</p>
+          </div>
+          <div className="sptbox">
+            <Link href={"/support"} className="supportlink">
+              Support
+            </Link>
+            <Link href={"/privacy"} className="privacylink">
+              Privacy
+            </Link>
+            <Link href={"/terms"} className="termslink">
+              Terms
+            </Link>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+}
+
+// function Footer() {
+//   return <footer></footer>;
+// }
+export default Footer;
